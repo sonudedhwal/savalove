5b0495950b961dadf8325b5e36edf91d1b0857ea
sonudedhwal <103274788+sonudedhwal@users.noreply.github.com>
2022-05-03 00:08:55 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$('.navToggle').on('click', function (e) {
+  e.preventDefault();
+  $('body').toggleClass('navToggleActive');
+});
+
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 10) {
+    $('body').addClass('fixedHeader');
+  } else {
+    $('body').removeClass('fixedHeader');
+  }
+});
+
+
+var swiper = new Swiper(".teamSwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".testimonialSlider", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
